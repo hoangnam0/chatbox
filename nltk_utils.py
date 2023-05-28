79318a2f85ba93ace3b7712a292c2091c292d34e
hoangnam0 <namhoi73@gmail.com>
2023-05-29 00:07:51 +0700

    nam

--- nltk_utils.py
+++ nltk_utils.py
@@ -16,7 +16,6 @@ def bag_of_words(tokenized_sentence, all_words):
     for idx, w in enumerate(all_words):
         if w in tokenized_sentence:
             bag[idx] = 1.0
-
     return bag
 
 # text = "Tôi muốn đặt một vé máy bay đi Hà Nội"
