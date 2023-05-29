b212cdadc95dba78d1b8f430c05a679fa3ad3c69
hoangnam0 <80726581+hoangnam0@users.noreply.github.com>
2023-05-29 11:27:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-
 from flask import Flask, render_template, request, jsonify
-
 from chat import get_response
-
-
 
 app = Flask(__name__)
 
-@app.get("/")
-def index_get():
+@app.route("/")
+def index():
     return render_template("base.html")
 
-@app.post("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
-    text = request.get_json().get('message')
-    # TODO: check if text is valid
+    data = request.get_json()
+    if data is None or "message" not in data:
+        return jsonify({"error": "Invalid request"}), 400
+
+    text = data["message"]
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
+
 if __name__ == "__main__":
     app.run()
-
-
